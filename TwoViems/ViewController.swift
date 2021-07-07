1d97e9995c69934174f485f3db2afa71ca6d9a62
Anton Grekov <grekov_anton@bk.ru>
2021-07-07 15:56:05 +0300

    Initial Commit

---
+++ TwoViems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViems
+//
+//  Created by Anton Grekov on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
